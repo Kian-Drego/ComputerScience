4e30ec11235a3f8c6b948b3bee743f97d4470851
kian-drego <drego.kian.boi@gmail.com>
2025-08-07 18:56:53 +0530

    Update Linear_Search.py

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,13 +1,13 @@
 arr = []
-demo = open("demo.txt", "r")
+demo = open("demo.txt", "r") # this opens the txt file
 
 for x in demo:
-    arr.append(int(x))
+    arr.append(int(x)) # append add the value of x to the next spot in the array
 print(arr)
 
 src = int(input("Enter the value you want to search: "))
 
-flag = False
+flag = False # used instead of else, else statement would give repeated statement "Number not Found"
 for x in arr:
     if int(x)==src:
         print("Number Found")
@@ -15,3 +15,5 @@ for x in arr:
 
 if flag == False:
     print("Number Not Found")
+
+demo.close # closes the txt file
