2d79ec5fd9f2c00d1956189b0fa70a790d4eae91
kian-drego <127304775+Kian-Drego@users.noreply.github.com>
2025-08-07 18:30:32 +0530

    Linear_Search With File Handling

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,2 +1,13 @@
-p = open("demo.txt", "r")
-print(p.read())
+arr = []
+print(arr)
+demo = open("demo.txt", "r")
+
+src = int(input("Enter the value you want to search: "))
+
+for x in demo:
+    arr.append(int(x))
+    if int(x)==src:
+        print("Number Found")
+    else print("Number Not Found")
+print(arr)
+
