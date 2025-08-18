5f19d1afd4f58aaebfebdeaee004cfecd91be0b3
kian-drego <drego.kian.boi@gmail.com>
2025-08-18 18:47:03 +0530

    file

--- A2 Notes/14 - Communication and Internet Technologies/14 - Communication and Internet Technologies.md
+++ A2 Notes/14 - Communication and Internet Technologies/14 - Communication and Internet Technologies.md
@@ -12,6 +12,8 @@
 - Structured in layers, with each layer responsible for a part of the communication process
 - Data packets transferred one by one from top layer to bottom layer before being set sent off
 - Each layer, which corresponds to specific rules and software to validate these rules operates independently
-![](Pasted%20image%2020250818182516.png)
-![](Pasted%20image%2020250818182500.png)
+
+TCP/IP Protocol
+- Enables computers to communicate over the internet
+
  
---
+++ Python/Linear_Search.py
@@ -0,0 +1,25 @@
+arr = []
+demo = open("demo.txt", "r") # this opens the txt file
+
+for x in demo:
+    arr.append(int(x)) # append add the value of x to the next spot in the array
+print(arr)
+
+src = int(input("Enter the value you want to search: "))
+
+flag = False # used instead of else, else statement would give repeated statement "Number not Found"
+for x in arr:
+    if int(x)==src:
+        print("Number Found")
+        flag = True
+
+if flag == False:
+    print("Number Not Found \n")
+
+demo.close # closes the txt file
+
+# An easier Search statement would be:
+if src in arr:
+    print("Updated Script: Number Found")
+else:
+    print("Updated Script: Number Not Found")
